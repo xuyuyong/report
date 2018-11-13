8e7c78477aafbce2960537db1047a80907e0b27d
xuyuyong <3176229153@qq.com>
2018-11-13 13:40:23 +0800

    页面

--- src/main/java/com/finance/report/controller/IndexAction.java
+++ src/main/java/com/finance/report/controller/IndexAction.java
@@ -19,6 +19,6 @@ public class IndexAction {
 
     @GetMapping("/index")
     public String index(){
-        return "dashboard";
+        return "main";
     }
 }
--- src/main/java/com/finance/report/controller/ReportAction.java
+++ src/main/java/com/finance/report/controller/ReportAction.java
@@ -1,9 +1,18 @@
 package com.finance.report.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  * @author xuyuyong
  * @create 2018-11-13 9:54
  * @content
  */
+@Controller
 public class ReportAction {
+
+    @GetMapping("/table")
+    public String index(){
+        return "emp/table_data_tables";
+    }
 }
